243ab0abdb70272515ed5bd6709b7e000ee2262b
kraxli <kraxli@pm.me>
2020-12-15 22:43:32 +0100

    improve snippets

--- pythonx/snippet_helpers.py
+++ pythonx/snippet_helpers.py
@@ -5,5 +5,5 @@ def file_most_recent(path):
     files = os.listdir(path)
     paths = [os.path.join(path, basename) for basename in files]
     file = max(paths, key=os.path.getctime)
-    return file
+    return os.path.basename(file)
 
